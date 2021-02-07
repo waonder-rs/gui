6574b493d5381ea5cfd7fd9c7ae2621f8fcecbba
Timothée Haudebourg <timothee.haudebourg@hemisemi.fr>
2021-02-07 16:08:47 +0100

    vulkano

--- shell.nix
+++ shell.nix
@@ -1,14 +1,10 @@
-with import <nixpkgs> {};
-
-runCommand "dummy" {
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
 	buildInputs = [
-		clang
-    libGL
-		wayland-protocols
-    wayland
-    #xorg.libX11
-		pkg-config
+		pkgs.vulkan-loader
+		pkgs.cmake
+		pkgs.python3
+		pkgs.shaderc
 	];
-	# LD_LIBRARY_PATH = "/nix/store/8z6xdhf6z2lb1y6h7380p3q8x2ya38d2-wayland-1.17.0/lib";
-	ELECTRON_TRASH = "gio";
-} ""
+	LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
+}
